b9c01640207b21455e561776d74d4211f370b24d
indrajith76 <indrajithgoswami@gmail.com>
2022-08-18 15:49:28 +0600

    commit with withdraw js

---
+++ js/withdraw.js
@@ -0,0 +1,9 @@
+document.getElementById('btn-withdraw').addEventListener('click', function(){
+    const newWithdrawAmount = getInputFieldValueById('withdraw-field');
+    const previousWithdrawTotal = getTextElementValueById('withdraw-total');
+    const newWithdrawTotal = newWithdrawAmount + previousWithdrawTotal;
+    setTextElementValueById('withdraw-total', newWithdrawTotal);
+    const previousBalanceTotal = getTextElementValueById('balance-total');
+    const newBalanceTotal = previousBalanceTotal - newWithdrawAmount;
+    setTextElementValueById('balance-total', newBalanceTotal);
+})
